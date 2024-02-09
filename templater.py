e5edecd83882bffbffd450010885f5fb8d947f1f
oakleal <Alexander.Oakley@cfs.com.au>
2024-02-09 18:31:20 +1100

    first

---
+++ templater.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+class BotInfo:
+    ...
+
+
+
+bot_id = "ID"
+priority = "Final Priority (Numbered Order)"
+bot_name = "Bot Name"
+description = "Description"
+technology = "Technology"
+App_Architecture = "App Architecture"
+Process = "Process"
+Applications = "Applications"
+Inputs = "Inputs"
+Outputs = "Outputs"
+Bot_author(s) = "Bot Author(s)"
+paths = "Path to main script file"
+Autobots_Project_ID = "Autobots Project ID"
+Usage Data Source = "Usage Data Source"
+Users of the bot (Business Team / Ops Team) = "Users of the bot (Business Team / Ops Team)"
+Demo Status = "Demo Status"
+Demo Status Date = "Demo Status Date"
+Demo Recording Link = "Demo Recording Link"
+Onboarded to TCS BPO = "Onboarded to TCS BPO"
+Owner = "Owner"
+Process Owner contact = "Process Owner' contact"
+Demonstration Contact = "Demonstration Contact"
